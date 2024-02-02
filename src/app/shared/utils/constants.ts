ea2fb73a37ea985314fb177ef7f90cfafb4b39ce
FacuGonzales <facundogonzalesliendo@gmail.com>
2024-02-02 20:25:00 -0300

    se crea constante

---
+++ src/app/shared/utils/constants.ts
@@ -0,0 +1,13 @@
+export const MOCK_MAIN_DATA = {
+  title: 'Texto principal',
+  text_secondary: 'Texto secundario texto secundario texto secundario texto secundario texto secundario texto secundario',
+  text_info: 'Texto sobre otro elemento',
+  text_button: 'botón',
+  icon: '../../assets/img/caritas-negras@2x.png'
+};
+
+
+export const MOCK_ERROR_GET_DATA = {
+  title: 'ERROR',
+  text_secondary: 'No pudimos obtener la informacion solicitada, por favor vuelve a intentarlo mas tarde. Disculpe las molestias',
+}
