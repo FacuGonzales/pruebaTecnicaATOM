cf1e80344549b27d719a163c8c3ff099d73d30b8
FacuGonzales <facundogonzalesliendo@gmail.com>
2024-02-07 15:28:32 -0300

    se agrega params

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -9,37 +9,19 @@ import { Observable } from 'rxjs';
 export class MoviesService {
   constructor(private http: HttpClient) { }
 
-  getDataMovies(data: any): Observable<any> {
+  getDataMovies(data: any, page: number = 1): Observable<any> {
     const paramsId =  data.id ? `i=${data.id}` : '';
     const paramsYear =  data.year ? `y=${data.year}` : '';
     const paramsTitle =  data.title ? `s=${data.title}` : '';
     const paramsType =  data.type ? `type=${data.type}` : '';
-    // getById i=imdbID
-
-    // getByTitle s=
-
-    // getByYears y=
-
-    // getByType type=movie, series, episode
+    const paramasPage = page ? `page=${page}` : 1;
 
     let sendParams = '';
-
-    if(paramsId) sendParams = `&${paramsId}`;
-
+    if(paramsId) sendParams = sendParams.concat(`&${paramsId}`);
     if(paramsYear) sendParams = sendParams.concat(`&${paramsYear}`);
     if(paramsTitle) sendParams = sendParams.concat(`&${paramsTitle}`);
     if(paramsType) sendParams = sendParams.concat(`&${paramsType}`);
 
-    // if(paramsYear) {
-    //   sendParams = sendParams ? sendParams.concat(`&${paramsYear}`) : `&${paramsYear}`;
-    // }
-
-    // const sendParams = (paramsId && paramsYear && paramsTitle && paramsType) ? `&${paramsId}&${paramsYear}&${paramsTitle}&${paramsType}` :
-    //                    (paramsYear && paramsTitle && paramsType) ? `&${paramsYear}&${paramsTitle}&${paramsType}` :
-    //                    (paramsTitle && paramsType) ? `&${paramsTitle}&${paramsType}` :
-    //                    (paramsType) ? `&${paramsType}` :
-    //                    '';
-
-    return this.http.get<any>(`${SERVICE_PATH.addresses.data}${sendParams}`);
+    return this.http.get<any>(`${SERVICE_PATH.addresses.data}&${paramasPage}${sendParams}`);
   }
 }
